60931675cc29fae9073dc810d8a134a243250aeb
Atticus Roach <MAtticusRoach@gmail.com>
2016-04-13 08:02:34 -0700

    first scripts.

---
+++ Assets/BasicSyntax.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicSyntax : MonoBehaviour 
+{
+
+
+	void Start () 
+	{
+		Debug.Log (transform.position.x);
+
+		if (transform.position.y <= 5f) 
+		{
+			Debug.Log ("watch out below!");
+		}
+
+	}
+}
